ae14bbaca7687b372663de64ad18331c8aa6d15c
Sven <thijssen_sven@hotmail.com>
2019-07-03 10:03:22 -0400

    comment indentation

--- src/instructions/Comment.py
+++ src/instructions/Comment.py
@@ -3,7 +3,7 @@ from instructions.Instruction import Instruction
 
 class Comment(Instruction):
     def __init__(self, comment):
-        super().__init__(comment=comment)
+        self.comment = comment
 
     def __str__(self):
-        return self.comment
+        return "| " + str(self.comment)
